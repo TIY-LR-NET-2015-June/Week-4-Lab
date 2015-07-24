3da3aae013887880b617484a99ffe32b0f281e8a
Michael Null <michael.null@bpmw.net>
2015-07-24 12:09:34 -0500

    removed types except for article, set up create page for article content

--- WebApplication1/WebApplication1/Models/Article.cs
+++ WebApplication1/WebApplication1/Models/Article.cs
@@ -7,9 +7,12 @@ namespace WebApplication1.Models
 {
     public class Article
     {
-        public Person Author { get; set; }
+        public string Author { get; set; }
+        public string AuthorPicture { get; set; }
         public string Headline { get; set; }
         public string Text { get; set; }
         public string Picture { get; set; }
+        public string Info { get; set; }
+        public int Rating { get; set; }
     }
 }
