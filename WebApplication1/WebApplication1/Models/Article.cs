7ad405a3f6a0366bbc5fc1ce7bef1e7387e5b754
Michael Null <michael.null@bpmw.net>
2015-07-26 22:30:23 -0500

    mostly done, maybe put a couple of finishing touches on to make it prettier

---
+++ WebApplication1/WebApplication1/Models/Article.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Article
+    {   
+        [Key]
+        public Guid Id { get; set; }
+
+        [DisplayName("Enter Your Name")]
+        [Required(ErrorMessage = "please let us now who you are")]
+        public string Author { get; set; }
+
+        [DisplayName("Enter a URL for your picture")]
+        public string AuthorPicture { get; set; }
+
+        [DisplayName("Enter a title for you post")]
+        [Required]
+        public string Headline { get; set; }
+
+        [DisplayName("Enter your post here")]
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Text { get; set; }
+
+        [DisplayName("Add a picture to your post to make it interesting")]
+        public string Picture { get; set; }
+
+        [DisplayName("Is this an advertisement for a product or service?")]
+        public bool isAdvertisement { get; set; }
+
+        [DisplayName("Date of your article")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0: yyyy MMM dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+    }
+}
