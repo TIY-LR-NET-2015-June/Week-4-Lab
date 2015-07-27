bae83d67a06e1508db9195fe63efce6646cc92fb
AaronHudson <aaron.hudson18@gmail.com>
2015-07-27 04:32:49 -0500

    Built post mechanisms

--- SurfandPaddle/SurfandPaddle/Controllers/HomeController.cs
+++ SurfandPaddle/SurfandPaddle/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SurfandPaddle.Models;
 
 namespace SurfandPaddle.Controllers
 {
@@ -24,6 +25,52 @@ namespace SurfandPaddle.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Posts post)
+        {
+            List<Posts> posts = new List<Posts>() { (Posts)HttpContext.Application["Posts"] };
+            post.ID = posts.Count + 1;
+            posts.Add(post);
+            HttpContext.Application["Posts"] = posts;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Posts post)
+        {
+            List<Posts> posts = new List<Posts>() { (Posts)HttpContext.Application["Posts"] };
+            posts[post.ID] = post;
+            HttpContext.Application["Posts"] = posts;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Posts post)
+        {
+            List<Posts> posts = new List<Posts>() { (Posts)HttpContext.Application["Posts"] };
+            posts.Remove(post);
+            HttpContext.Application["Posts"] = posts;
             return View();
         }
     }
