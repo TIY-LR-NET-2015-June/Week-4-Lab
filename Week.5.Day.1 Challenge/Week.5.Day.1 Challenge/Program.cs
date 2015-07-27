f050d3ebc5243724f01b01f204657bef604897a5
Scott Ferguson <esfergus@gmail.com>
2015-07-27 17:28:49 -0500

    Disabled javascript causing error. Added requirements to post class.

---
+++ Week.5.Day.1 Challenge/Week.5.Day.1 Challenge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week._5.Day._1_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileContents = System.IO.File.ReadAllText(@"C:\TIY\Projects\Week5.Day1\names.txt").Split(',').ToList();
+            fileContents.Sort();
+
+            for (int i = 0; i < fileContents.Count(); i++)
+            {
+                Names ns = new Names();
+                ns.Name = fileContents[i].Trim('\"');
+            }
+            Console.WriteLine("Total Score = {0}", names.Sum(x => x.);
+        }
+
+    }
+    class Names
+    {
+        public string Name { get; set; }
+        public int LetterScore
+        {
+            get
+            {
+                int score = 0;
+                var array = Name.ToUpper().ToCharArray();
+                foreach (var letter in array)
+                {
+                    score += letter - 'A' + 1;
+                }
+                return score;
+            }
+        }
+    }
+    class SetValue
+    {
+        public int MyProperty { get; set; }
+    }
+    
+}
--- Week4-Lab/Week4-Lab/Models/Post.cs
+++ Week4-Lab/Week4-Lab/Models/Post.cs
@@ -8,13 +8,19 @@ namespace Week4_Lab.Models
 {
     public class Post
     {
+        [Required]
+        [MaxLength(100)]
+        [Display(Name ="Post Title")]
         public string Title { get; set; }
+
         [DataType (DataType.MultilineText)]
         public string Text { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime PostDate { get; set; }
+
         public string Author { get; set; }
+
         public int ID { get; set; }
     }
 }
