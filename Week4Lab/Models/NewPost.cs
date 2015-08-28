1fa228803572e908c6e3ecca5e8b7941bb4d0a46
Jason W <williams.jasonmd@gmail.com>
2015-08-28 15:13:54 -0500

    Finished Project

---
+++ Week4Lab/Models/NewPost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Week4Lab.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+        public DateTime DatePosted { get; set; }
+    }
+}
